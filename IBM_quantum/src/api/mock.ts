d6d6c9a9709844206c7cdc771a93a41aa368d597
unknown <migel.sorokin@yandex.ru>
2025-11-11 01:37:38 +0300

    Lab 6 init.

--- IBM_quantum/src/api/mock.ts
+++ IBM_quantum/src/api/mock.ts
@@ -6,8 +6,14 @@ export const MOCK_GATES: IGate[] = [
     ID_gate: 1,
     Title: "Identity Gate",
     Description: "Не изменяет состояния кубита.",
+    Status: true, 
+    Image: "http://127.0.0.1:9000/ibm-pictures/img/imageError.gif",
+    I0j0: 0,
+	  I0j1:	0,
+	  I1j0: 0,
+	  I1j1: 0,
+	  Matrix_koeff: "non",
     FullInfo: "Ничего не делает с состоянием кубита. Оставляет его без изменений.",
-    Image: "http://127.0.0.1:9000/ibm-pictures/img/I-gate.png",
     TheAxis: "non"
   },
   {
@@ -15,16 +21,28 @@ export const MOCK_GATES: IGate[] = [
     Title: "Pauli-X Gate (NOT gate)",
     Description: "Инвертирует состояние кубита.",
     FullInfo: "Аналог классического NOT-гейта. Переворачивает состояние кубита.",
-    Image:  "http://127.0.0.1:9000/ibm-pictures/img/X-gate.png",
-    TheAxis: "non"
+    Image:  "http://127.0.0.1:9000/ibm-pictures/img/imageError.gif",
+    TheAxis: "non",
+    Status: true, 
+    I0j0: 0,
+	  I0j1:	0,
+	  I1j0: 0,
+	  I1j1: 0,
+	  Matrix_koeff: "non",
   },
   {
     ID_gate: 3,
     Title: "X-axis Rotation Gate",
     Description: "Вращает кубит вокруг оси X на угол тэта.",
     FullInfo: "Эта операция вращает состояние кубита на сфере Блоха вокруг оси X. Значение угла поворота можно задать при компановке выражения (в деталях калькуляции).",
-    Image: "http://127.0.0.1:9000/ibm-pictures/img/X-rot-gate.png",
-    TheAxis: "X"
+    Image: "http://127.0.0.1:9000/ibm-pictures/img/imageError.gif",
+    TheAxis: "X",
+    Status: true, 
+    I0j0: 0,
+	  I0j1:	0,
+	  I1j0: 0,
+	  I1j1: 0,
+	  Matrix_koeff: "non",
   }
 ];
 
